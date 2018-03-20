60858b22009bd3fc79bac32332b47f87a92fa8aa
MrScriptX <dan.liebault@viacesi.fr>
2018-03-20 17:14:14 +0100

    mise a jour de la db ajout de nouvelle commande protection mineur

--- Discord_Bot/Module/ActionMessage.cs
+++ Discord_Bot/Module/ActionMessage.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,13 @@ namespace Discord_Bot.Module
     [Group("do")]
     public class ActionMessage : ModuleBase<SocketCommandContext>
     {
+        //biffle command
+        [Command("biffle")]
+        public async Task DoAutoBiffle()
+        {
+            await ReplyAsync($"{ Context.User.Mention } se biffle!");
+        }
+
         [Command("biffle")]
         public async Task DoBiffle(SocketGuildUser user)
         {
@@ -23,12 +31,22 @@ namespace Discord_Bot.Module
             await ReplyAsync($"{ Context.User.Mention } jette des cacahuetes a { user.Mention }");
         }
 
+
+        //doigt command
+        [Command("doigt")]
+        public async Task DoJul()
+        {
+            await ReplyAsync($"{ Context.User.Mention } fait le signe de Jul...Quel merde https://media2.giphy.com/media/afqT2ykIlYcVi/200w.gif");
+        }
+
         [Command("doigt")]
         public async Task DoDoigt(SocketGuildUser user)
         {
             await ReplyAsync($"{ Context.User.Mention } fait un doigt a { user.Mention }");
         }
 
+
+        //olive command
         [Command("olive")]
         public async Task Olive()
         {
@@ -45,6 +63,25 @@ namespace Discord_Bot.Module
         public async Task DoOliveA(String nom)
         {
             await ReplyAsync($"{ Context.User.Mention } fait une olive a { nom }");
+        }
+
+        //calin command
+        [Command("calin")]
+        public async Task DoCalin()
+        {
+            await ReplyAsync($"{ Context.User.Mention } me fait un calin! C'est mignon https://media3.giphy.com/media/7hW7hXXri33NK/200w.gif");
+        }
+
+        [Command("calin")]
+        public async Task DoCalinA(SocketGuildUser user)
+        {
+            await ReplyAsync($"{ Context.User.Mention } fait un calin a { user.Mention }!");
+        }
+
+        [Command("test")]
+        public async Task TestCommand()
+        {
+            
         }
     }
 }
--- Discord_Bot/Module/Random.cs
+++ Discord_Bot/Module/Random.cs
@@ -6,19 +6,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Security.Cryptography;
+using Discord;
 
 namespace Discord_Bot.Module
 {
     public class Random : ModuleBase<SocketCommandContext>
     {
-        [Command("Random")]
+        System.Random rnd = new System.Random();
+
+        [Command("random")]
         public async Task RandomTake()
         {
+            if(!Context.Channel.IsNsfw)
+            {
+                await ReplyAsync("Pas ici, on va nous voir :kissing_heart:");
+                return;
+            }
+
             string link = "";
             int ID = 0;
 
-            System.Random rnd = new System.Random();
-            ID = rnd.Next(1, 46);
+            ID = rnd.Next(1, 84);
+            Console.WriteLine(ID);
 
             using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\scrip\\source\\repos\\Discord_Bot\\Discord_Bot\\Database1.mdf;Integrated Security=True"))
             using (SqlCommand cmd = new SqlCommand("SELECT link FROM [dbo].[Table] WHERE Id='" + ID + "';", connection))
